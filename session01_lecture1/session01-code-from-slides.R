e1762fbedd4fbcf0071b42f1ba632bc6dd08dce7
maxcheatle <mcheatle.mim2023@london.edu>
2023-05-24 23:04:28 +0100

    v_homework2

--- session01_lecture1/session01-code-from-slides.R
+++ session01_lecture1/session01-code-from-slides.R
@@ -154,4 +154,12 @@ UA_flights <- flights %>%
 (
   UA_flights <- flights %>% 
   filter(carrier == "UA")
+  
 )
+
+glimpse(flights)
+
+flights %>%
+  group_by(carrier) %>%
+  summarise(mean_dep_delay = mean(dep_delay, na.rm = TRUE), mean_arr_delay = mean(arr_delay, na.rm = TRUE), count = n()) %>% 
+  arrange(desc(count))
